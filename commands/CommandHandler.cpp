bcc699628f3ac25c6a1525c626fb036ad2a821c6
noussel <noussaiba.elkhadar@usmba.ac.ma>
2026-05-10 14:53:13 +0100

    JOIN encours

--- commands/CommandHandler.cpp
+++ commands/CommandHandler.cpp
@@ -4,18 +4,37 @@
 #include "../includes/Server.hpp"
 #include "../includes/Commandeparse.hpp"
 
+// static void valideArgs(std::vector<std::string> args)
+// {
+//     std::cout << "-------> " << args[1] << "\n";
+//     if(args.size() > 1 && args[1][0] == '#' && args[1][1] && isalnum(args[1][1])){
+//         for(size_t j = 0; args[1][j]; j++){
+//             if(args[1][j] == ','){
+//                 if(args[1][j+1] && args[1][j+1] == '#'){
+//                     if(args[1][j+2] && isalnum(args[1][j+2]))
+//                         continue;
+//                     else throw "invalide channel name (empty or not alphaNum)";
+//                 }         
+//                 else throw "coma (,) folowed by invalide character";
+//             }
+//         }
+//     }
+//     else throw "Channels need '#' at start";
+// }
+
+
 static void valideArgs(std::vector<std::string> args)
 {
-    std::cout << "-------> " << args[1] << "\n";
-    if(args.size() > 1 && args[1][0] == '#' && args[1][1] && isalnum(args[1][1])){
-        for(size_t j = 0; args[1][j]; j++){
-            if(args[1][j] == ','){
-                if(args[1][j+1] && args[1][j+1] == '#'){
-                    if(args[1][j+2] && isalnum(args[1][j+2]))
-                        continue;
-                    else throw "invalide channel name (empty or not alphaNum)";
+    std::cout << "-------> " << args[0] << "\n";
+    if(args[0][0] == '#' && args[0][1] && isalnum(args[0][1])){
+        for(size_t j = 1; args[0][j]; j++){
+            if(args[0][j] == '#'){
+                if(args[0][j+1] && isalnum(args[0][j+1]) && args[0][j-1] == ','){
+                    // if(args[0][j+2] && isalnum(args[0][j+2]))
+                    continue;
+                    // else throw "invalide channel name (empty or not alphaNum)";
                 }         
-                else throw "coma (,) folowed by invalide character";
+                else throw "INVALIDE CHANNELS NAME";
             }
         }
     }
@@ -33,24 +52,27 @@ void leaveAll(std::vector<Channel*> ch,Client *client)
 
 std::vector<std::string> split_Channels(std::string chnl){//#ch1,#ch2,
     std::vector<std::string> Splited_chnl;
-    for(size_t i = 0; i < chnl.size(); i++)
-    {
-        if(chnl[i] == '#')
-        {
-            i++;
-            size_t next = chnl.find('#', i);
-            if(next == std::string::npos)
-                break;
-            std::string name = chnl.substr(i, next-1 - i);// -2 to not take ','                 #abc,#ch
-            Splited_chnl.push_back(name);
-        }
-    }
+    std::string str;
+    std::getline(chnl, str, '')
+    // for(size_t i = 0; i < chnl.size(); i++)
+    // {
+    //     if(chnl[i] == '#')
+    //     {
+    //         i++;
+    //         size_t next = chnl.find('#', i);
+    //         if(next == std::string::npos)
+    //             next = chnl.length() + 1;
+    //         std::string name = chnl.substr(i, next-1 - i);// -2 to not take ','                 #abc,#ch
+    //         Splited_chnl.push_back(name);
+    //     }
+    // }
     return Splited_chnl;
 }
 
+
 std::vector<std::string> split_Keys(std::vector<std::string> keys){//key1 key2
     std::vector<std::string> Splited_keys;
-    for(size_t i = 2; i < keys.size(); i++)
+    for(size_t i = 1; i < keys.size(); i++)
     {
         Splited_keys.push_back(keys[i]);
     }
@@ -106,7 +128,7 @@ void join_Multi_Channls(std::vector<std::string> channels, std::vector<std::stri
 
 void Client::HandledJOIN(const Commandeparse &cmd)
 {
-    //CHACK IF INVITE ONLY 
+    //CHECK IF INVITE ONLY 
     try{
 
         if(!Authenticated){
@@ -119,15 +141,23 @@ void Client::HandledJOIN(const Commandeparse &cmd)
 
         valideArgs(cmd.args);//if no # //existe 
 
-        if(cmd.args.size() == 2 && cmd.args[1] == "0")
+        if(cmd.args.size() == 2 && cmd.args[0] == "0")
         {
             leaveAll(ser->get_all_channels(), this);
         }
 
         else 
         {//MODE #secret +k hello42   //key == MODE #secret +k hello42
-            std::vector<std::string> channels = split_Channels(cmd.args[1]);
+            std::vector<std::string> channels = split_Channels(cmd.args[0]);
+            for(size_t i = 0; i < channels.size(); i++){
+                std::cout << RED << channels[i] << RESET << "  ";
+            }
+            std::cout << "\n";
             std::vector<std::string> keys = split_Keys(cmd.args);
+            for(size_t i = 0; i < keys.size(); i++){
+                std::cout << GRE << keys[i] << RESET << "  ";
+            }
+            std::cout << "\n";
             join_Multi_Channls(channels, keys, this,  ser);//if first client creat channel
         }
     }
